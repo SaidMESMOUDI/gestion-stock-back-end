f6d175d5ae4d9a9d501533549dcdfcbd9d9cfa1b
saidMESMOUDI <saidmsz1@gmail.com>
2020-12-22 13:01:11 +0100

    first commit

---
+++ src/main/java/com/saidus/gestionstockbackend/GestionstockbackendApplication.java
@@ -0,0 +1,13 @@
+package com.saidus.gestionstockbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionstockbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionstockbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/saidus/gestionstockbackend/GestionstockbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saidus.gestionstockbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionstockbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
